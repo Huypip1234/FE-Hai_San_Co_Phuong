b6fbb707f7c7f8f165638604abc2d8c3fb3106f8
huypip1234 <huy123nn69hz@gmail.com>
2024-02-28 18:15:24 +0700

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,14 +5,14 @@ import { defaultImage } from "@/constants";
 const Loading = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center fixed inset-0 z-[99999] bg-white flex-col gap-[0.5rem]">
-      <CustomImage
+      {/* <CustomImage
         alt="banner"
         width={200}
         height={200}
         src={defaultImage}
         className="rounded-xl transition-all duration-300 ease-in-out animate-bounce max-sm:w-[100px] max-sm:h-[100px]"
-      />
-      <p className="font-[700] text-[14px] sm:text-[24px] text-primary">
+      /> */}
+      <p className="font-[700] text-[24px] sm:text-[36px] text-primary transition-all duration-300 ease-in-out animate-bounce ">
         Hải Sản Cô Phương
       </p>
     </div>
