14a99b122df46df9b753d4c90586cbc2a8b05f97
Huypip1234 <huy123nn69hz@gmail.com>
2024-03-01 00:22:30 +0700

    ok

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="bg-slate-100 mt-[5rem]">
       <div className="layout-container py-[3rem]">
-        <div className="grid grid-cols-1 gap-[1rem] sm:grid-cols-2 items-center">
+        <div className="grid grid-cols-1 gap-[1rem] md:grid-cols-2 items-center">
           <div>
             <h1 className="text-primary font-[700] text-[18px] sm:text-[24px]">
               Hải Sản Cô Phương
@@ -46,35 +46,33 @@ const Footer = () => {
 
           {isMounted && (
             <div>
-              <div>
-                <div id="fb-root"></div>
-                <script
-                  async
-                  defer
-                  crossOrigin="anonymous"
-                  src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v19.0&appId=411097243526646"
-                  nonce="dN8soO0F"
-                ></script>
-                <div
-                  className="fb-page"
-                  data-href="https://www.facebook.com/haisancophuong1974/"
-                  data-tabs=""
-                  data-width=""
-                  data-height=""
-                  data-small-header="false"
-                  data-adapt-container-width="true"
-                  data-hide-cover="false"
-                  data-show-facepile="true"
+              <div id="fb-root"></div>
+              <script
+                async
+                defer
+                crossOrigin="anonymous"
+                src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v19.0&appId=411097243526646"
+                nonce="dN8soO0F"
+              ></script>
+              <div
+                className="fb-page"
+                data-href="https://www.facebook.com/haisancophuong1974/"
+                data-tabs=""
+                data-width=""
+                data-height=""
+                data-small-header="false"
+                data-adapt-container-width="true"
+                data-hide-cover="false"
+                data-show-facepile="true"
+              >
+                <blockquote
+                  cite="https://www.facebook.com/haisancophuong1974/"
+                  className="fb-xfbml-parse-ignore"
                 >
-                  <blockquote
-                    cite="https://www.facebook.com/haisancophuong1974/"
-                    className="fb-xfbml-parse-ignore"
-                  >
-                    <a href="https://www.facebook.com/haisancophuong1974/">
-                      Hải Sản Cô Phương
-                    </a>
-                  </blockquote>
-                </div>
+                  <a href="https://www.facebook.com/haisancophuong1974/">
+                    Hải Sản Cô Phương
+                  </a>
+                </blockquote>
               </div>
             </div>
           )}
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import CustomPagination from "@/components/custom/CustomPagination";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
   useEffect(() => {
@@ -20,6 +21,8 @@ export default function Home() {
 
     window.scrollTo(0, 0);
   }, []);
+
+  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
 
   return (
     <div className="layout-container  mt-[1rem]">
@@ -39,14 +42,18 @@ export default function Home() {
 
       <div className="max-w-[1100px] mx-auto my-[2rem] ">
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-[1rem] sm:gap-[1.5rem] ">
-          {[...new Array(24)].map((_item, index) => (
-            <ProductItem
-              data-aos="zoom-in"
-              data-aos-duration="1000"
-              data-aos-delay="200"
-              key={index}
-            />
-          ))}
+          {[...new Array(24)].map((_item, index) =>
+            isMobile && index < 4 ? (
+              <ProductItem key={index} />
+            ) : (
+              <ProductItem
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+                data-aos-delay="200"
+                key={index}
+              />
+            )
+          )}
         </div>
         <div className="flex justify-end mt-[2rem]">
           <CustomPagination defaultCurrent={1} total={50} responsive />
