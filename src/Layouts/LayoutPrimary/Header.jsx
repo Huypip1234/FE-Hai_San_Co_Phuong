5ba2619ec75d4ddfe9b0d63ca5a42586a355028f
Huypip1234 <huy123nn69hz@gmail.com>
2024-03-10 22:54:05 +0700

    search

--- src/Layouts/LayoutPrimary/Header.jsx
+++ src/Layouts/LayoutPrimary/Header.jsx
@@ -47,6 +47,7 @@ const Header = () => {
           <CloseOutlined
             onClick={() => {
               setIsShowSearch(false);
+              handleSearch("");
             }}
             className="sm:hidden text-[20px]"
           />
